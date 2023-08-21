d63ba2fd4266afdf94a53c126a3c405409baa9c4
SarabjitS <35809229+SarabjitS@users.noreply.github.com>
2023-08-21 11:45:47 -0230

    made the first move using append()

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
-console.log(document.querySelector("#opening1"));
+const item1 = document.querySelector("#opening1");
 const item2 = document.querySelector("#opening2");
 const item3 = document.querySelector("#opening3");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const chessBoard = document.getElementById("chess-board");
+const squareE2 = document.getElementById("sq-e2");
+const squareE4 = document.getElementById("sq-e4");
 
 let moves = 0;
 
-item1.addEventListener("click", playButton(), false);
+item1.addEventListener("click", playButton);
 
 function playButton() {
-  const prevBtn = document.querySelector("#prev");
-  const nextBtn = document.querySelector("#next");
-  console.log("test");
+  // prevBtn.addEventListener("click", prevHandler);
+  nextBtn.addEventListener("click", nextHandler);
 }
-
-prevBtn.addEventListener("click", prevHandler, false);
-nextBtn.addEventListener("click", nextHandler, false);
 
 function nextHandler() {
-  if (moves < 4) {
-    countUp();
-    addActive(moves);
-    console.log(moves);
-  } else {
-    console.log("Already checkmate");
-  }
+  squareE4.append(squareE2.childNodes[1]);
 }
 
-function prevHandler() {
-  if (moves > 0) {
-    removeActive(moves);
-    countDown();
-    console.log(moves);
-  } else {
-    console.log("Back to the start");
-  }
-}
+// function nextHandler() {
+//   if (moves < 4) {
+//     countUp();
+//     addActive(moves);
+//     console.log(moves);
+//   } else {
+//     console.log("Already checkmate");
+//   }
+// }
 
-function addActive(moves) {
-  const activeMove = document.querySelector(".move" + moves);
-  activeMove.classList.add("active");
-}
+// function prevHandler() {
+//   if (moves > 0) {
+//     removeActive(moves);
+//     countDown();
+//     console.log(moves);
+//   } else {
+//     console.log("Back to the start");
+//   }
+// }
 
-function removeActive(moves) {
-  const activeMove = document.querySelector(".move" + moves);
-  activeMove.classList.remove("active");
-}
+// function addActive(moves) {
+//   const activeMove = document.querySelector(".move" + moves);
+//   activeMove.classList.add("active");
+// }
 
-function countUp() {
-  return moves++;
-}
-
-function countDown() {
-  return moves--;
-}
+// function removeActive(moves) {
+//   const activeMove = document.querySelector(".move" + moves);
+//   activeMove.classList.remove("active");
+// }
