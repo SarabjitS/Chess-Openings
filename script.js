23c0c0a5a484609778d7e0d6c7fad47dcfc0c245
SarabjitS <35809229+SarabjitS@users.noreply.github.com>
2023-08-22 07:53:06 -0230

    removed some comments and unnecessary classes

--- script.js
+++ script.js
@@ -9,67 +9,38 @@ const nextBtn = document.querySelector("#next");
 const chessBoard = document.getElementById("chess-board");
 const squareE2 = document.getElementById("sq-e2");
 const squareE4 = document.getElementById("sq-e4");
-const squareC2 = document.getElementById("sq-c2");
+const squareC7 = document.getElementById("sq-c7");
 const squareC5 = document.getElementById("sq-c5");
-const squareG7 = document.getElementById("sq-g7");
-const squareB2 = document.getElementById("sq-b2");
-const squareD2 = document.getElementById("sq-d2");
+const squareG8 = document.getElementById("sq-g8");
+const squareB1 = document.getElementById("sq-b1");
+const squareD7 = document.getElementById("sq-d7");
 const squareF2 = document.getElementById("sq-f2");
 const squareF4 = document.getElementById("sq-f4");
 const squareD5 = document.getElementById("sq-d5");
 const squareF6 = document.getElementById("sq-f6");
 const squareC3 = document.getElementById("sq-c3");
 
-// let moves = 0;
-
 const toiletVariation = [
   [squareE2, squareE4],
-  [squareC2, squareC5],
+  [squareC7, squareC5],
   [squareF2, squareF4],
-  [squareG7, squareF6],
-  [squareB2, squareC3],
-  [squareD2, squareD5],
+  [squareG8, squareF6],
+  [squareB1, squareC3],
+  [squareD7, squareD5],
 ];
 
 item1.addEventListener("click", playButton);
 
 function playButton() {
-  // prevBtn.addEventListener("click", prevHandler);
-  nextBtn.addEventListener("click", function () {
-    nextHandler(toiletVariation[0][0], toiletVariation[0][1]);
-  });
+  for (let i = 0; i < toiletVariation.length; i++) {
+    nextBtn.addEventListener("click", function () {
+      nextHandler(toiletVariation[i][0], toiletVariation[i][1]);
+
+      console.log(toiletVariation[i][0], toiletVariation[i][1]);
+    });
+  }
 }
 
 function nextHandler(sourceMove, destinationMove) {
   destinationMove.append(sourceMove.childNodes[1]);
 }
-
-// function nextHandler() {
-//   if (moves < 4) {
-//     countUp();
-//     addActive(moves);
-//     console.log(moves);
-//   } else {
-//     console.log("Already checkmate");
-//   }
-// }
-
-// function prevHandler() {
-//   if (moves > 0) {
-//     removeActive(moves);
-//     countDown();
-//     console.log(moves);
-//   } else {
-//     console.log("Back to the start");
-//   }
-// }
-
-// function addActive(moves) {
-//   const activeMove = document.querySelector(".move" + moves);
-//   activeMove.classList.add("active");
-// }
-
-// function removeActive(moves) {
-//   const activeMove = document.querySelector(".move" + moves);
-//   activeMove.classList.remove("active");
-// }
